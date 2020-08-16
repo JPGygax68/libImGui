64505778e603f6e6b5acf521ef12f5d86ad80666
Jean-Pierre Gygax <gygax@practicomp.ch>
2020-08-16 21:49:31 +0200

    Trying to make InputText more comfortable to use

--- libs/c++-latest/include/imgui/imgui-c++-latest.h
+++ libs/c++-latest/include/imgui/imgui-c++-latest.h
@@ -1,7 +1,9 @@
 #pragma once
 
 #include <string_view>
+#include <string>
 #include <functional>
+#include <limits>
 
 #include <imgui/imgui.h>
 #include <imgui/imgui_internal.h>
@@ -11,12 +13,12 @@
     use of modern features of new versions of C++.
 
     No other extensions should be added here - that is left for other modules, which hovewer
-    will then in turn required those same C++ features.
+    will then in turn require those same C++ features.
  */
 
 namespace ImGui {
 
-    /** Namespace-based variant of ImGuiInputTextFlags 
+    /** Namespace-based variant of ImGuiInputTextFlags
      */
     struct InputTextFlags {
 
@@ -49,15 +51,57 @@ namespace ImGui {
 
     static_assert(sizeof(ImGui::InputTextFlags) <= sizeof(int));
 
+    // Convert several data types to either a label or an ID
+
+    template <typename T>
+    auto to_label(T value) { return std::to_string(value); }
+
+    template <>
+    auto to_label(const char *label) { return label; }
+
+
     // INPUTTEXT VARIANTS
 
     template <size_t BufferSize, typename UserData>
-    bool InputText(const char *label, char (&buffer)[BufferSize], ImGui::InputTextFlags flags,
+    bool InputText(const char *label, char *buffer, size_t bufferSize, ImGui::InputTextFlags flags = 0,
         int (*callback)(ImGuiInputTextCallbackData*) = nullptr,
-        UserData *user_data = nullptr
+        UserData* user_data = nullptr
     ) {
 
         return ImGui::InputText(label, buffer, BufferSize, (int)flags, callback, user_data);
+    }
+
+    template <typename UserData>
+    bool InputText(const std::string& label, char* buffer, size_t b_size, ImGui::InputTextFlags flags = 0,
+        int (*callback)(ImGuiInputTextCallbackData*) = nullptr,
+        UserData* user_data = nullptr
+    ) {
+
+        return ImGui::InputText(label.c_str(), buffer, b_size, (int)flags, callback, user_data);
+    }
+
+    template <typename Label, size_t BufferSize, typename UserData>
+    bool InputText(Label label, char (&buffer)[BufferSize], ImGui::InputTextFlags flags = 0,
+        int (*callback)(ImGuiInputTextCallbackData*) = nullptr,
+        UserData *user_data = nullptr
+    ) {
+    
+        return ImGui::InputText(label, buffer, BufferSize, (int)flags, callback, user_data);
+    }
+
+    template <typename ID, typename UserData = void>
+    bool InputText(ID id, std::string& text, ImGui::InputTextFlags flags = 0,
+        int (*callback)(ImGuiInputTextCallbackData*) = nullptr,
+        UserData* user_data = nullptr
+    ) {
+        std::vector<char> buffer( std::max(text.capacity(), (size_t)512) );
+        strcpy_s(buffer.data(), buffer.size(), text.c_str());
+        if (ImGui::InputText(to_label(id), buffer.data(), buffer.size(), (int)flags, callback, user_data)) {
+            text = buffer.data();
+            return true;
+        }
+        else
+            return false;
     }
 
     // TODO: more overloads, and also for InputTextEx()
@@ -65,7 +109,7 @@ namespace ImGui {
     // TODO: move into separate "utils" module!
 
     void BringItemIntoView() {
-        
+
         ScrollToBringRectIntoView(GetCurrentWindow(), { GetItemRectMin(), GetItemRectMax() });
     }
 
